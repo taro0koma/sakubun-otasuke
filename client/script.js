fa5112c8e2b1f5022adc72f0159e4ddad1f8dbdb
yuriwolf <19800714>
2024-05-05 15:29:49 +0900

    update the server url

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
 
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://sakubun-otasuke.com/',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
