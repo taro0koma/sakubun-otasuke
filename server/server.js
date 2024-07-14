a9140cf07496cfabb2c470007b056136e56f0b63
comaco <taro0koma@gmail.com>
2024-07-14 19:28:29 +0900

    add a route

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//表現ぴったり探し用のURL
 app.get('/',async (req,res) => {
   res.status(200).send({
     message: 'Hello from CodeX'
@@ -32,6 +33,36 @@ app.post('/',async (req,res) =>{
         {
           "role": "user",
           "content": `「${prompt}」という言葉をゆたかに表現するため、日本の作文に使われそうな1文の文章を10パターン用意してください。1個目は、どれくらいか程度を表すもの。2個目は、動作を含めた表現にしてください。3個目は、心の中のつぶやきを含めた表現にしてください。4個目は、過去の気持ちで表現してください。5個目は、感情を強調した表現にしてください。6個目は、音や様子を含めて表現してください。7個目は、心の中の叫びのように表現してください。8個目は、何かに影響されて心が変化する様子を含めて下さい。9個目は、無意識で何かをしてしまう様子を含めて下さい。10個目は、自分の表情を含めて表現して下さい。また、一つ一つに改行を入れて下さい。もし、その言葉が方言（関西弁など）であれば文章もそれに合わせた方言に変えてください。`
+        }
+      ],
+      temperature: 0.2,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0
+    });
+
+    res.status(200).send({
+      bot: response.choices[0].message.content
+    })
+  }catch (error){
+    console.log(error);
+    res.status(500).send({ error })
+  }
+})
+
+//段落の組み立て用のURL
+app.post('/danraku',async (req,res) =>{
+
+  try{
+    const prompt = req.body.prompt;
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          "role": "user",
+          "content": prompt
         }
       ],
       temperature: 0.2,
