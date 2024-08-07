2e79ed81039b957de4cb76c989526ab6a4171000
comaco <taro0koma@gmail.com>
2024-08-07 18:01:37 +0900

    iroiro

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-
-//           "content": `「${prompt}」という言葉をゆたかに表現するため、日本の作文に使われそうな1文の文章を10パターン用意してください。1個目は、どれくらいか程度を表すもの。2個目は、動作を含めた表現にしてください。3個目は、心の中のつぶやきを含めた表現にしてください。4個目は、過去の気持ちで表現してください。5個目は、感情を強調した表現にしてください。6個目は、音や様子を含めて表現してください。7個目は、心の中の叫びのように表現してください。8個目は、何かに影響されて心が変化する様子を含めて下さい。9個目は、無意識で何かをしてしまう様子を含めて下さい。10個目は、自分の表情を含めて表現して下さい。また、一つ一つに改行を入れて下さい。もし、その言葉が関西弁であれば文章も関西弁にしてください（絶対！！）`
+//import文-----------------------------------------------------------------------
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import OpenAI from 'openai';
 
+//--------------------------------------------------------------------------------
 dotenv.config();
 
 const openai = new OpenAI({
@@ -15,24 +15,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//表現ぴったり探し用のURL
+
+//処理------------------------------------------------------------------------
+
+//表現ぴったり探し用
 app.get('/',async (req,res) => {
   res.status(200).send({
     message: 'Hello 表現'
   })
 });
-
 app.post('/',async (req,res) =>{
 
   try{
-    const sakka = req.body.sakka;
     const prompt = req.body.prompt;
-
+    //ここにgradeをとし、作家名に変換する配列を書く
     const response = await openai.chat.completions.create({
-      model: "gpt-4o",
+      model: "gpt-4o-mini",
       messages: [
         {
-          "role": "system","content":`作家の${sakka}`,
+          "role": "system","content":`作家のあまんきみこ`,
           "role":"user","content":`「${prompt}」という言葉をゆたかに表現するため、日本の作文に使われそうな1文の文章を10パターン用意してください。1個目は、どれくらいか程度を表すもの。2個目は、動作を含めた表現にしてください。3個目は、心の中のつぶやきを含めた表現にしてください。4個目は、過去の気持ちで表現してください。5個目は、感情を強調した表現にしてください。6個目は、音や様子を含めて表現してください。7個目は、心の中の叫びのように表現してください。8個目は、何かに影響されて心が変化する様子を含めて下さい。9個目は、無意識で何かをしてしまう様子を含めて下さい。10個目は、自分の表情を含めて表現して下さい。また、一つ一つに改行を入れて下さい。`
         }
       ],
@@ -52,19 +53,35 @@ app.post('/',async (req,res) =>{
   }
 })
 
-//段落の組み立て用のURL
-app.post('/danraku',async (req,res) =>{
+//表現ぴったり探し以外のとき用-----------------------------------------------
 
+app.post('/danraku',async (req,res) =>{
   try{
-    const sakka = req.body.sakka;
+    const grade = req.body.gakunen;
+    const grades = {
+      s1: ["7歳","松谷みよ子"],
+      s2: ["8歳","あんびるやすこ"],
+      s3: ["9歳","安西水丸"],
+      s4: ["10歳","角野栄子"],
+      s5: ["11歳","宮沢賢治"],
+      s6: ["12歳","ヨシタケシンスケ"],
+      t1: ["13歳","新見南吉"],
+      t2: ["14歳","重松清"],
+      t3: ["15歳","森絵都"],
+      k1: ["16歳","住野よる"],
+      k2: ["17歳","小川洋子"],
+      k3: ["18歳","梨木香歩"],
+      oldPeople: ["大人","あさのあつこ"],
+    }
+
     const prompt = req.body.prompt;
 
     const response = await openai.chat.completions.create({
       model: "gpt-4o",
       messages: [
         {
-          "role": "system","content":`作家の${sakka}`,
-          "role":"user","content": prompt
+          "role": "system","content":`作家の${grades[grade][1]}`,
+          "role":"user","content": `${grades[grade][0]}向けにしてください。${prompt}`
         }
       ],
       temperature: 0.2,
