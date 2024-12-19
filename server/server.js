70941bf2e4129162e9573a5954e466a4a137b645
comaco <taro0koma@gmail.com>
2024-12-19 17:37:14 +0900

    できますように..

--- server/server.js
+++ server/server.js
@@ -23,15 +23,7 @@ const allowedOrigins = [
   'https://react-sakubun-otasuke.vercel.app/',
 ]
 
-app.use(cors({
-  origin: function (origin,callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1 ) {
-      callback(null,true);
-    }else {
-      callback(new Error('Not allowed by cors...'));
-    }
-  }
-}));
+app.use(cors());
 app.use(express.json());
 
 
