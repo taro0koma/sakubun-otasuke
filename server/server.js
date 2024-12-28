152f8c80721201850f2d9f8fd7bce3738ea68dee
comaco <taro0koma@gmail.com>
2024-12-28 14:53:02 +0900

    表現ぴったり探しもAzureOpenAIにした！（できないかもしれないけど）

--- server/server.js
+++ server/server.js
@@ -197,5 +197,85 @@ app.post('/api/azure',async (req,res) => {
 }
 })
 
+app.get('/api/azurehyougen',async (req,res) => {
+  res.status(200).send({
+    message: 'Hello 表現'
+  })
+});
+app.post('/api/azurehyougen',async (req,res) =>{
+
+  try{
+    console.log(endpoint);
+    console.log('サーバーサイドに入ったよ');
+    const client = new AzureOpenAI({
+      deployment,apiVersion,apiKey,endpoint
+    });
+  
+    const prompt = req.body.prompt;
+    console.log(prompt);
+  
+  
+    const response = await client.chat.completions.create({
+      messages: [
+        { role: "system", content: '作家のあまんきみこ' },
+        { role: "user", content: `「${prompt}」という言葉をゆたかに表現するため、日本の作文に使われそうな1文の文章を10パターン用意してください。1個目は、どれくらいか程度を表すもの。2個目は、動作を含めた表現にしてください。3個目は、心の中のつぶやきを含めた表現にしてください。4個目は、過去の気持ちで表現してください。5個目は、感情を強調した表現にしてください。6個目は、音や様子を含めて表現してください。7個目は、心の中の叫びのように表現してください。8個目は、何かに影響されて心が変化する様子を含めて下さい。9個目は、無意識で何かをしてしまう様子を含めて下さい。10個目は、自分の表情を含めて表現して下さい。また、一つ一つに改行を入れて下さい。` }
+      ],
+    });
+  
+    console.log("Azure-apiの中に入ったよ")
+  
+    res.status(200).send({
+      bot: response.choices[0].message.content
+    })
+  }catch{
+    res.status(500).send({ error })
+  }
+  })
+  
+  app.get('/api/azurehyougen',async (req,res) => {
+    res.status(200).send({
+      message: 'Hello 表現'
+    })
+  });
+  app.post('/api/azurehyougen',async (req,res) =>{
+  
+    try{
+      const prompt = req.body.prompt;
+      //GASの定時実行のための分岐
+      if (prompt === 'just say "A"!') {
+        console.log("GASからの定時実行");
+        res.status(200).send({
+          bot: "GASからの定時実行"
+        })
+        return;
+      }
+      
+      const user_prompt = `「${prompt}」という言葉をゆたかに表現するため、日本の作文に使われそうな1文の文章を10パターン用意してください。1個目は、どれくらいか程度を表すもの。2個目は、動作を含めた表現にしてください。3個目は、心の中のつぶやきを含めた表現にしてください。4個目は、過去の気持ちで表現してください。5個目は、感情を強調した表現にしてください。6個目は、音や様子を含めて表現してください。7個目は、心の中の叫びのように表現してください。8個目は、何かに影響されて心が変化する様子を含めて下さい。9個目は、無意識で何かをしてしまう様子を含めて下さい。10個目は、自分の表情を含めて表現して下さい。また、一つ一つに改行を入れて下さい。`;
+      
+      //ここにgradeをとし、作家名に変換する配列を書く
+      const response = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+          {
+            "role": "system","content":`作家のあまんきみこ`,
+            "role":"user","content":user_prompt
+          }
+        ],
+        temperature: 0.2,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0
+      });
+  
+      res.status(200).send({
+        bot: response.choices[0].message.content
+      })
+    }catch (error){
+      console.log(error);
+      res.status(500).send({ error })
+    }
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => console.log('サーバーは動いています！ポート：http://localhost:5000'));
